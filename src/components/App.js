2aefb5677f6587a2248ddae91e8acce89ac67361
Tuhin1904 <iamtuhinn@gmail.com>
2025-01-06 18:36:24 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,26 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import TodosList from "./TodosList";
 
 const App = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn Javascript.", completed: false },
+    { id: 2, text: "Learn React.", completed: false },
+    { id: 3, text: "Learn Node.", completed: false },
+  ]);
+  const handleComplete = (id) => {
+    setTodos((prev) => {
+      return prev.map((item) =>
+        id == item.id ? { ...item, completed: true } : item
+      );
+    });
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <TodosList todos={todos} handleComplete={handleComplete} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/TodosList.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const TodosList = ({ todos, handleComplete }) => {
+  return (
+    <>
+      <h2>Child Component</h2>
+      <ul>
+        {todos.map((item) => (
+          <li key={item.id}>
+            {item.text}{" "}
+            {!item.completed && (
+              <button onClick={() => handleComplete(item.id)}>Complete</button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default TodosList;
